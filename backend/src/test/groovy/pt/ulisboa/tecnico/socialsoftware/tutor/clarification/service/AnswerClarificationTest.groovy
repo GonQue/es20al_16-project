c6ea4f6d73ea4fd5bb6552805bd6e1d710c49a64
itzchkm8 <joao.dias12321@gmail.com>
2020-03-08 11:59:03 +0000

    test cases defined
    test cases defined #6

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/service/AnswerClarificationTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/service/AnswerClarificationTest.groovy
@@ -1,4 +1,64 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.clarification.service
 
-class AnswerClarificationTest {
+import pt.ulisboa.tecnico.socialsoftware.tutor.clarification.domain.QuestionClarification
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import spock.lang.Specification
+
+class AnswerClarificationTest extends Specification{
+
+    def clarification
+
+    def setup() {
+        clarification = new QuestionClarification()
+    }
+
+    def 'the teacher clarifies the student'() {
+        // the teacher clarifies the student
+        expect: false
+    }
+
+    def 'the teacher with the id does not exist'() {
+        // an exception is thrown
+        expect: false
+    }
+
+    def 'the clarification already exists'() {
+        // an exception is thrown
+        expect: false
+    }
+
+    def 'status is NOT_ANSWERED'() {
+        // an exception is thrown
+        expect: false
+    }
+
+    def 'status is null'() {
+        // an exception is thrown
+        expect: false
+    }
+
+    def 'status is blank'() {
+        // an exception is thrown
+        expect: false
+    }
+
+    def 'teacher id is null'() {
+        // an exception is thrown
+        expect: false
+    }
+
+    def 'teacher id is blank'() {
+        // an exception is thrown
+        expect: false
+    }
+
+    def 'teacher response is null'() {
+        // an exception is thrown
+        expect: false
+    }
+
+    def 'teacher response is blank'() {
+        // an exception is thrown
+        expect: false
+    }
 }
