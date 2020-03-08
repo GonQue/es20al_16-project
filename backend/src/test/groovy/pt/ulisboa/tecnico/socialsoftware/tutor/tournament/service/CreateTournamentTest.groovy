7aedb612399ec7faa9077fcf09b84ae4b3d6c899
rap1999 <rap1999@live.com.pt>
2020-03-08 23:22:01 +0000

    Closed #10 Test cases for createTournament defined
    added 2 more tests

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/CreateTournamentTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/CreateTournamentTest.groovy
@@ -1,17 +1,59 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
 import spock.lang.Specification
 
 class CreateTournamentTest extends Specification {
 
+    def tournamentService
 
     def setup(){
-
+        tournamentService = new TournamentService();
     }
 
-    def "create a tournament"(){
-        // create a tournament given a name, start and end time, question number,
+    def "create a tournament with 2 topics"(){
+        // create a tournament with a name, creator, start and end time, 2 topics, number of questions,
+        expect: false
     }
 
+    def "create tournament with 0 topics"(){
+        // create a tournament with 0 topics
+        expect: false
+    }
+
+    def "tournament creator is not a student"(){
+        //throw exception
+        expect: false
+    }
+
+    def "student is not in course execution of tournament"(){
+        //throw exception
+        expect: false
+    }
+
+    def "End time before start time"(){
+       //throw exception
+        expect: false
+    }
+
+    def "create tournament with 0 questions"(){
+        //throw exception
+        expect: false
+    }
+
+    def "create tournament with null topics"(){
+        //throw exception
+        expect: false
+    }
+    
+    def "tournament name is empty"(){
+        //throw exception
+        expect: false
+    } 
+    
+    def "tournament name is blank"(){
+        //throw exception
+        expect: false
+    }
 
 }
