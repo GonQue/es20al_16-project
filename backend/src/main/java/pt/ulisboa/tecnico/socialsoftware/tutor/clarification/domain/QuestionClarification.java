eb05c2c961a6e5781207bd2e6001d984537aade3
itzchkm8 <joao.dias12321@gmail.com>
2020-03-09 17:00:35 +0000

    default to both implementations

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/domain/QuestionClarification.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/domain/QuestionClarification.java
@@ -1,7 +1,104 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.clarification.domain;
 
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question;
+import java.time.LocalDateTime;
 
-public class QuestionClarification extends Question {
+public class QuestionClarification {
+    public enum Status {
+        ANSWERED, NOT_ANSWERED
+    }
 
+    private Integer id;
+    private Integer key;
+    private Integer questionId;
+    private String content;
+    private String status;
+    private LocalDateTime creationDate = null;
+    private Integer teacherId;
+    private String teacherResponse;
+    private LocalDateTime responseDate = null;
+
+    public QuestionClarification() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Integer questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherResponse() {
+        return teacherResponse;
+    }
+
+    public void setTeacherResponse(String teacherResponse) {
+        this.teacherResponse = teacherResponse;
+    }
+
+    public LocalDateTime getResponseDate() {
+        return responseDate;
+    }
+
+    public void setResponseDate(LocalDateTime responseDate) {
+        this.responseDate = responseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionDto{" +
+                "id=" + id +
+                "questionId" + questionId +
+                ", content='" + content + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
