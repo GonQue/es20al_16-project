f39f31196b59fbce9abeb52232c94c24bba56194
rap1999 <rap1999@live.com.pt>
2020-03-11 16:32:09 +0000

    Close #14 implemented Tests, created class Tournament TournamentDto TournamentService

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
@@ -1,7 +1,17 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.tournament;
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.COURSE_EXECUTION_NOT_FOUND;
+
 public class TournamentService {
 
+   public TournamentDto createTournament(int executionId, TournamentDto tournamentDto){
+       //CourseExecution courseExecution = courseExecutionRepository.findById(executionId).orElseThrow(() -> new TutorException(COURSE_EXECUTION_NOT_FOUND, executionId));
 
+      return new TournamentDto();
+   }
 
 }
