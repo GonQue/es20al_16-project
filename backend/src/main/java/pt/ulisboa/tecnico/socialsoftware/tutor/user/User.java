ca1e99f16d3be5f3aa48851b09b089af50fd9c7e
Rafael Poças <41762896+PoCyfr@users.noreply.github.com>
2020-03-13 16:05:39 +0000

    Tdp (#40)
    * created domain dto service createTournamentTest classes
    
    * fix #9 case tests defined
    
    * #10 Test cases for createTournament defined
    
    * #10 added 2 more tests
    
    * Close #14 implemented Tests, created class Tournament TournamentDto TournamentService
    
    * Closed #15 Implemented service, dto, class Tournament and relations with other classes
    
    * Closed #10 Test cases for createTournament defined
    
    added 2 more tests
    
    * Closed #14 implemented Tests, created class Tournament TournamentDto TournamentService
    
    * Closed #15 Implemented service, dto, class Tournament and relations with other classes, created exceptions
    
    * #18 implementation of case tests
    
    * #31 Implemented service, changed test cases to use repository
    
    * fix #15 fixed tests
    
    * Closed #15 Implemented service, dto, class Tournament and relations with other classes, created exceptions
    
    fix #15 fixed tests
    
    * Closed #32 Service refactoring, new test using unroll, extracted functions from createTournament()
    
    * #34 refactored service and added test
    
    * add tournament to creator
    
    * resolved conflits
    
    * P1 file
    
    Co-authored-by: joanna-isabbel <joana.i.ferreira@tecnico.ulisboa.pt>

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/user/User.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/user/User.java
@@ -72,6 +72,7 @@ public class User implements UserDetails, Importable {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "teacher", orphanRemoval=true)
     private List<ClarificationResponse> clarification_responses = new ArrayList<>();
 
+
     public User() {
     }
 
