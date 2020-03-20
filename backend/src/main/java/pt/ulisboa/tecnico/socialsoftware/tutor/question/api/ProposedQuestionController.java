7ec4f7c765088419c72e31a19be733aa670ce72b
GonQue <gonsanke@outlook.com>
2020-03-20 17:30:20 +0000

    Created ProposedQuestionController - #50

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/ProposedQuestionController.java
@@ -0,0 +1,25 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.QuestionProposalService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.ProposedQuestionDto;
+
+import javax.validation.Valid;
+
+@RestController
+public class ProposedQuestionController {
+
+    @Autowired
+    private QuestionProposalService questionProposalService;
+
+    @PostMapping("/courses/{courseId}/questions/proposed-questions")
+    @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#courseId)")
+    public ProposedQuestionDto createProposedQuestion(@PathVariable Integer courseId, @Valid @RequestBody ProposedQuestionDto propQuestionDto) {
+        return questionProposalService.studentSubmitQuestion(courseId, propQuestionDto);
+    }
+}
