667745f9f87420ee256171112a212a921f448089
rap1999 <rap1999@live.com.pt>
2020-03-21 19:13:07 +0000

    Created controller method for tournament creation closes #61

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
@@ -0,0 +1,30 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
+
+@RestController
+public class TournamentController {
+    private static Logger logger = LoggerFactory.getLogger(TournamentController.class);
+
+    @Autowired
+    private TournamentService tournamentService;
+
+    @PostMapping("/executions/{executionId}/tournaments")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or (hasRole('ROLE_STUDENT') and hasPermission(#executionId, 'EXECUTION.ACCESS'))")
+    public TournamentDto createTournament(@PathVariable int executionId, @RequestBody TournamentDto tournamentDto){
+        logger.debug("createTournament");
+        return tournamentService.createTournament(executionId, tournamentDto);
+    }
+
+
+
+
+}
