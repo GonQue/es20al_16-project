9e90ac1e7743234450ffa1977b43500d07058036
rap1999 <rap1999@live.com.pt>
2020-03-24 23:10:28 +0000

    close #60 Jmeter tests and groovy class to test web service for tournament creation

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/CreateTournamentsServiceSpockPerformanceTest.groovy
@@ -0,0 +1,88 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.Quiz
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.dto.QuizDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.repository.QuizRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.repository.TournamentRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.dto.UserDto
+import spock.lang.Specification
+
+@DataJpaTest
+class CreateTournamentsServiceSpockPerformanceTest extends Specification {
+
+    @Autowired
+    TournamentService tournamentService
+
+    @Autowired
+    TournamentRepository tournamentRepository
+
+    @Autowired
+    CourseExecutionRepository courseExecutionRepository
+
+    @Autowired
+    UserRepository userRepository
+
+    @Autowired
+    QuizRepository quizRepository
+
+    @Autowired
+    TopicRepository topicRepository
+
+    def "performance test to create 10000 course executions"() {
+        given:"a course execution"
+        def course = new Course("COURSE_NAME", Course.Type.TECNICO)
+        def courseExecution = new CourseExecution(course, "ACRONYM", "ACADEMIC_TERM", Course.Type.TECNICO)
+        courseExecutionRepository.save(courseExecution)
+
+        and: "a tournament dto"
+        def tournamentDto = new TournamentDto()
+        tournamentDto.setName("TOURNAMENT 1 TESTE PERFORMANCE")
+        def user = new User('name', "STUDENT_USERNAME", 1, User.Role.STUDENT)
+        user.addCourse(courseExecution)
+        userRepository.save(user)
+        def userDto = new UserDto(user)
+        tournamentDto.setCreator(userDto)
+
+        def quiz = new Quiz()
+        quiz.setKey(1)
+        quizRepository.save(quiz)
+        def quizDto = new QuizDto(quiz, false)
+        tournamentDto.setQuiz(quizDto)
+
+        def topicOne = new Topic()
+        topicRepository.save(topicOne)
+        def TopicDto = new TopicDto(topicOne)
+        tournamentDto.setTopics(new ArrayList<>(Arrays.asList(TopicDto)))
+        tournamentDto.setNumberOfQuestions(2)
+        when:
+        1.upto(100000, { tournamentService.createTournament(courseExecution.getId(), tournamentDto)})
+
+        then:
+        true
+    }
+
+
+    @TestConfiguration
+    static class TournamentServiceImplTestContextConfiguration{
+
+        @Bean
+        TournamentService tournamentService(){
+            return new TournamentService()
+        }
+
+    }
+}
