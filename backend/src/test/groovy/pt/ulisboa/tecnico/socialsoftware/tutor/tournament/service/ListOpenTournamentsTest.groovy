e9f63b71e7898b188f67199229b914b34d043f6e
joanna-isabbel <joana.i.ferreira@tecnico.ulisboa.pt>
2020-03-24 14:08:49 +0000

    Definicao testes listar torneios abertos

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/ListOpenTournamentsTest.groovy
@@ -0,0 +1,23 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import spock.lang.Specification
+
+class ListOpenTournamentsTest extends Specification {
+
+    def setup(){}
+    def "no open tournaments"(){
+        //returns empty
+    }
+
+    def "one open tournaments"(){
+        //returns tournaments
+    }
+
+    def "three open tournaments"(){
+        //returns tournaments-
+    }
+
+    def "closed tournament on the list"(){
+        //throws exception
+    }
+}
