9fb155cf8630c23b2cbea500a8b1713536049937
Rafael Poças <41762896+PoCyfr@users.noreply.github.com>
2020-04-19 22:57:14 +0100

    Tdp creation (#142)
    * #130 First cypress test to create tournament working
    
    * closes #130 3 more tests, 2 with errors, wrong date and numberOfQuestions, 1 sucessfull, 2 tournaments multiple topics
    
    * P3.md
    
    * Close #130 createTournament.js tests that were not commited due to gitignore
    
    * gitignore change
    
    * fix prettier warnings
    
    * P3

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -57,7 +57,7 @@ Cypress.Commands.add('deleteCourseExecution', (acronym) => {
 })
 
 Cypress.Commands.add('createFromCourseExecution', (name, acronym, academicTerm) => {
-    cy.contains(name)
+    cy.get(td).contains(name)
         .parent()
         .should('have.length', 1)
         .children()
@@ -69,3 +69,53 @@ Cypress.Commands.add('createFromCourseExecution', (name, acronym, academicTerm) 
     cy.get('[data-cy="saveButton"]').click()
 })
 
+
+
+
+
+Cypress.Commands.add('demoStudentLogin', () => {
+    cy.visit('/')
+    cy.get('[data-cy="studentButton"]').click()
+})
+Cypress.Commands.add('createTournament', (name, topics, day1, day2, nextMonth, pickQuestionNumber) => {
+    cy.get('[data-cy="createButton"]').click()
+
+    //Name
+    cy.get('[data-cy="Name"]').type(name)
+
+    //Start date
+    cy.get('[data-cy=startDate]').click()
+    cy.get('button').contains(day1).click().wait(500)
+    cy.contains('OK').click().wait(500)
+
+    //End date
+    cy.get('[data-cy=endDate]').click()
+    if(nextMonth)
+        cy.get('i[class="v-icon notranslate mdi mdi-chevron-right theme--light"]:visible').click().wait(500)
+    cy.get('button:visible').contains(day2).click().wait(500)
+    cy.get('button:visible').contains('OK').click()
+
+    //Number of questions
+    if(pickQuestionNumber) cy.get('[data-cy=numberOfQuestions]').click()
+
+    //Topics
+    cy.get('[data-cy="topics"]').click();
+    topics.forEach(function(topics){
+        cy.contains(topics).click().wait(100);
+    });
+
+    //Save
+    cy.get('[data-cy="saveButton"]').click().wait(200);
+})
+
+Cypress.Commands.add('checkTournament', (name, numberOfTournaments) => {
+    cy.contains(name)
+      .parent()
+      .should('have.length', numberOfTournaments)
+      .children()
+      .should('have.length', 4)
+})
+Cypress.Commands.add('removeTournamentFromDB', (name) => {
+    cy.exec('PGPASSWORD=123 psql -d tutordb -U rafa -h localhost -c "DELETE FROM tournaments_topics WHERE tournaments_id in(select id from tournaments where name=\'$name\')"', {env: {name: name}})
+    cy.exec('PGPASSWORD=123 psql -d tutordb -U rafa -h localhost -c "DELETE FROM tournaments WHERE name=\'$name\'"', {env: {name: name}})
+})
