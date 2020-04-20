472bfd95cf7c157b53d3e51934b8ab82ebc01781
Gonçalo Querido <32230042+GonQue@users.noreply.github.com>
2020-04-20 01:47:38 +0100

    Merge reference (#148)
    * jmeter test and fix getToken in TokenProvider
    
    * define access control
    
    * getExtenalCourseExecutions JMeter tests
    
    * getExtenalCourseExecutions JMeter tests
    
    * remove admin access
    
    * define DEMO_ADMIN role
    
    * Simplified statement DTOs, fixed answering error
    
    * Fix iOS scrolling and overlay on toolbar
    
    * JMeter Load test for create external course executios ws
    
    * add database teardown to JMeter tests
    
    * add performance test
    
    * View student answers to quizzes
    
    * clean code
    
    * clean code
    
    * fixes
    
    * SonarLinted, Quiz results for teacher, added clock to in_class quiz
    
    * update dependencies
    
    * X marks the spot
    
    * create course execution mock testing
    
    * Moved clock to the left, added quiz start date to list, reverted markdown editor
    
    * timer improvements, more stats for teacher views
    
    * CoursesView implementation
    
    * Evaluation quizzes without backsies and in available list
    
    * Update README.md
    
    * remove EntityManager references
    
    * create and remove course execution ui
    
    * small fixes admin functionality
    
    * small fixes admin functionality
    
    * Update README.md
    
    * cypress tests of admin of course executions
    
    * Update dependencies
    
    * add * to correct question closes #107
    
    Signed-off-by: Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
    
    * add oneWay and onlyQRCode properties to Quiz
    
    * onlyQRCode quizzes become available after scan
    
    * Timer stops once user leaves scan view
    
    * add JMeter test to create question
    
    * small syntactic fixes
    
    * Conflict resolved
    
    * Fenix change of repository
    
    * Update build.yml
    
    * refactor question key generation
    
    * refactor export questions applying the visitor pattern
    
    * visitor to generate quiz and question in latex
    
    * export quiz latex ui
    
    * Removed setId(), Removed usages of findByUsername from services
    
    * return zip file for quiz export
    
    * Revert "Removed setId(), Removed usages of findByUsername from services"
    
    This reverts commit 3ef9813518dc177ff8289864692a1c8876d0aced.
    
    * support several students in development
    
    * revert several demo students in dev
    
    * generate csv file for quiz
    
    * export course questions
    
    * fix lint warnings
    
    * fix name of image url
    
    * update package.json
    
    * reset demo
    
    * Prevent Question edition in frontend
    
    * Show-question-dialog bugfix, update dependencies, sanitizing html
    
    * fix image being sanitized
    
    * refactoring
    
    * cypress to tests folder
    
    Co-authored-by: Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
    Co-authored-by: Pedro Correia <pedro.correia.105@gmail.com>
    Co-authored-by: ruiprada <50016191+ruiprada@users.noreply.github.com>
    Co-authored-by: Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>

--- frontend/tests/e2e/support/commands.js
+++ frontend/tests/e2e/support/commands.js
@@ -25,6 +25,54 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 /// <reference types="Cypress" />
 Cypress.Commands.add('demoAdminLogin', () => {
+<<<<<<< HEAD
+  cy.visit('/');
+  cy.get('[data-cy="adminButton"]').click();
+  cy.contains('Administration').click();
+  cy.contains('Manage Courses').click();
+});
+
+Cypress.Commands.add('createCourseExecution', (name, acronym, academicTerm) => {
+  cy.get('[data-cy="createButton"]').click();
+  cy.get('[data-cy="Name"]').type(name);
+  cy.get('[data-cy="Acronym"]').type(acronym);
+  cy.get('[data-cy="AcademicTerm"]').type(academicTerm);
+  cy.get('[data-cy="saveButton"]').click();
+});
+
+Cypress.Commands.add('closeErrorMessage', (name, acronym, academicTerm) => {
+  cy.contains('Error')
+    .parent()
+    .find('button')
+    .click();
+});
+
+Cypress.Commands.add('deleteCourseExecution', acronym => {
+  cy.contains(acronym)
+    .parent()
+    .should('have.length', 1)
+    .children()
+    .should('have.length', 7)
+    .find('[data-cy="deleteCourse"]')
+    .click();
+});
+
+Cypress.Commands.add(
+  'createFromCourseExecution',
+  (name, acronym, academicTerm) => {
+    cy.contains(name)
+      .parent()
+      .should('have.length', 1)
+      .children()
+      .should('have.length', 7)
+      .find('[data-cy="createFromCourse"]')
+      .click();
+    cy.get('[data-cy="Acronym"]').type(acronym);
+    cy.get('[data-cy="AcademicTerm"]').type(academicTerm);
+    cy.get('[data-cy="saveButton"]').click();
+  }
+);
+=======
     cy.visit('/')
     cy.get('[data-cy="adminButton"]').click()
     cy.contains('Administration').click()
@@ -171,3 +219,4 @@ Cypress.Commands.add('deleteClarificationResponse', (questionContent, responseCo
 
 
 
+>>>>>>> origin/develop
