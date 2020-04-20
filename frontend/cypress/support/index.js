a0e0a758d0c33be408bac66873486faa798457f4
rap1999 <rap1999@live.com.pt>
2020-04-20 01:39:02 +0100

    changes tests folder, minor fixes

--- frontend/cypress/support/index.js
+++ frontend/cypress/support/index.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import '../../tests/e2e/support/commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
